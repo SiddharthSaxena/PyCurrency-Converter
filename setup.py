537389f986ee52d920ef839e97a63a3c01c8636c
SiddharthSaxena <siddharth.saxena25@gmail.com>
2016-11-23 15:13:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 setup(
-  name='PyCurrency',
-  packages=['PyCurrency'],
-  version='1.0.0',
-  description='A library to convert currency.',
-  author='Siddharth Saxena',
-  author_email='siddharth.saxena25@gmail.com',
-  url='https://github.com/SiddharthSaxena/PyCurrency',
-  download_url='https://github.com/SiddharthSaxena/PyCurrency',
-  keywords=['currency converter google'],
-  classifiers=[],
+    name='PyCurrency',
+    packages=['PyCurrency'],
+    version='1.0.0',
+    description='A library to convert currency.',
+    author='Siddharth Saxena',
+    author_email='siddharth.saxena25@gmail.com',
+    url='https://github.com/SiddharthSaxena/PyCurrency',
+    download_url='https://github.com/SiddharthSaxena/PyCurrency',
+    keywords=['currency converter google'],
+    classifiers=[],
 )
