b98c05ec8dd2e99c139cb2b12e4590d111817fa7
SiddharthSaxena <siddharth.saxena25@gmail.com>
2016-11-23 17:15:42 +0530

    Update setup files

--- setup.py
+++ setup.py
@@ -22,6 +22,5 @@ setup(
                  'Programming Language :: Python :: 3.2',
                  'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5'
-                 ]
+                 'Programming Language :: Python :: 3.5']
 )
