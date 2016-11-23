deb762ae6761b500bc4f4389a5ff8d468ba212ca
SiddharthSaxena <siddharth.saxena25@gmail.com>
2016-11-23 14:06:06 +0530

    Update Currency Converter.py

--- src/Currency Converter.py
+++ src/Currency Converter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import pycurrency
+import PyCurrency
 
 To = input('Convert To: ')
 amount = input('Amount: ')
 From = input('Convert from: ')
 
-print(pycurrency.get(amount, From, To))
+print(PyCurrency.get(amount, From, To))
