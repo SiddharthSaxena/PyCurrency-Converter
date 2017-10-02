77e275d66451134f001a1d80609b1d155cb1d66f
Siddharth Saxena <SiddharthSaxena@users.noreply.github.com>
2017-10-02 18:36:13 +0530

    Update new Google URL

--- PyCurrency_Converter/PyCurrency.py
+++ PyCurrency_Converter/PyCurrency.py
@@ -27,7 +27,7 @@ except ImportError:
 class PyCurrency:
     @staticmethod
     def convert(amount, _from, _to):
-        url = 'https://www.google.com/finance/converter?a={}&from={}&to={}'.format(amount, _from, _to)
+        url = 'https://finance.google.com/finance/converter?a={}&from={}&to={}'.format(amount, _from, _to)
         response = urllib2.urlopen(url)
         html = response.read()
         parsed = BeautifulSoup(html, 'lxml').body.find('span', attrs={'class': 'bld'}).text
@@ -35,7 +35,7 @@ class PyCurrency:
 
     @staticmethod
     def codes():
-        url = 'https://www.google.com/finance/converter'
+        url = 'https://finance.google.com/finance/converter'
         response = urllib2.urlopen(url)
         html = response.read()
         parser = BeautifulSoup(html, 'lxml').body.find_all('option')
